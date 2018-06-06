434593417c2aa6addcb0d926046f658cfd682cec
hvkcoder <hvkcoder@163.com>
2018-06-06 17:35:42 +0800

    project init

---
+++ README.md
@@ -0,0 +1,15 @@
+## Datetime Utils
+
+Datetime-utils is a js toolkit for manipulating Date objects.
+
+## Install
+
+```bash
+  $ npm install datetime-utils
+```
+
+## Features
+
+- [x] Conversion date
+
+## Usage
---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+require('./lib/date-extend');
+var dateCalculation = require('./lib/date-calculation');
+
+/**
+ * Conversion date
+ * @param { String || Number } datetime
+ * @return {Date}
+ */
+function convertDate(datetime) {
+  try {
+    if (typeof datetime !== "number") {
+      // Clear the space before and after the string
+      datetime = datetime.replace(/^(\s|\u00A0)+/, '').replace(/(\s|\u00A0)+$/, '');
+      // Format the date format
+      datetime = datetime.replace("/Date(", "")
+        .replace(")/", "")
+        .split("+")[0];
+    }
+    return new Date(datetime);
+  } catch (e) {
+    throw e;
+  }
+};
+
+/**
+ * Date format method
+ * @param {String || Number} datetime
+ * @param {String} fmt
+ * @return {String}
+ */
+function formate(datetime, fmt) {
+  // Determine if it is a Date type
+  if (datetime instanceof Date) {
+    return datetime.formate(fmt);
+  }
+  return convertDate(datetime).formate(fmt);
+};
+
+
+/**
+ * Date Calculation
+ * @param {String} type
+ * @param {Number} num
+ */
+function dateCalculation(type, num) {
+
+};
+
+/**
+ * Exports method
+ */
+exports = module.exports = {
+  calcType: dateCalculation.calcType,
+};
