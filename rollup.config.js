28d4722468c5306839389dfc0e7393b4a0a84497
hvkcoder <hvkcoder@163.com>
2018-07-10 13:52:30 +0800

    Add .babelrc

--- rollup.config.js
+++ rollup.config.js
@@ -1,2 +1,13 @@
-'use strict'
+'use strict';
 
+const path = require('path');
+
+export default {
+	input: path.join(__dirname, './src/index.js'),
+	output: {
+		file: path.join(__dirname, './utils-date.js'),
+		format: 'umd'
+	},
+	plugins: {
+	}
+}
