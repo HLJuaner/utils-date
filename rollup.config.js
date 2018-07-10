407dacc4127b2e22635d346b190a62eda28fbc09
hvkcoder <hvkcoder@163.com>
2018-07-10 14:59:01 +0800

    Update package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,33 @@
 'use strict';
 
+const version = require('./package.json').version;
 const path = require('path');
+const babel = require('rollup-plugin-babel');
+const cjs = require('rollup-plugin-commonjs');
+const uglify = require('rollup-plugin-uglify').uglify;
+
+// console.log(uglify());
+// 打包后生成 js 的顶部注释说明
+const banner =
+	`/*!\n` +
+	` * utils-date v${version}\n` +
+	` * (c) 2018-${new Date().getFullYear()} Slience HVK\n` +
+	` */`;
 
 export default {
-	input: path.join(__dirname, './src/index.js'),
+	input: path.join(__dirname, './src/utils-date.js'),
 	output: {
-		file: path.join(__dirname, './utils-date.js'),
-		format: 'umd'
+		file: path.join(__dirname, './dist/utils-date.min.js'),
+		banner: banner,
+		format: 'umd',
+		sourceMap: 'inline',
+		name: 'utilsDate'
 	},
-	plugins: {
-	}
+	plugins: [
+		cjs(),
+		babel({
+			exclude: 'node_modules/**' // 忽略 node_modules 的代码
+		}),
+		uglify()
+	]
 }
---
+++ src/utils-date.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export default () => {
+	console.log("This is the source code of jquery source code analysis series");
+}
