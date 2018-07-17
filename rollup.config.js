7a002d3b8e2403ce49c6df7ebd88e6d5e6616674
hvkcoder <hvkcoder@163.com>
2018-07-17 09:51:20 +0800

    update rollup.cofnig.js

--- rollup.config.js
+++ rollup.config.js
@@ -6,12 +6,27 @@ const babel = require('rollup-plugin-babel');
 const cjs = require('rollup-plugin-commonjs');
 const uglify = require('rollup-plugin-uglify').uglify;
 
+const resolve = p => path.join(__dirname, '.', p);
+
 // 打包后生成 js 的顶部注释说明
 const banner =
 	`/*!\n` +
 	` * utils-date v${version}\n` +
 	` * (c) 2018-${new Date().getFullYear()} Slience HVK\n` +
 	` */`;
+
+const builds = {
+	"utils-date-dev": {
+		entry: resolve('src/utils-date.js'),
+		dest: resolve('dist/utils-date.js'),
+		format: 'umd',
+		env: 'development',
+		banner
+	},
+	"utils-date-prod": {
+		banner
+	}
+}
 
 export default {
 	input: path.join(__dirname, './src/utils-date.js'),
